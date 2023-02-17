d940dfb7efde3ea23bb8b24e2603048f9f0bc4ef
riomonet <arizablozki@gmail.com>
2023-02-16 23:18:21 -0500

    Reset all files to beginning

--- calculator-test.js
+++ calculator-test.js
@@ -1,30 +1,11 @@
 
-
-//describe("Validation tests for  Main form =>", function() {
-
-
-    
-    // it('should have a valid form object', function () {
-    // 	expect(init_form()).toBeTrue();
-    // });
-
-
-    
-    // it('should get a  valid LOAN AMOUNT from the form', function () {
-    // 	// loan amount must be of type integer with a greater than $1000.00 but less than $500,000,000.00
-    // });
-
-
-    // it('should calculate the monthly rate correctly', function () {
-
-    // 	values = {amount: 1000000, years: 25, rate: 6};
-    // 	expect(calculateMonthlyPayment(values)).toEqual('6443.01');
-    
-    // });
-
-// });
-
-it('should return true if form exists', function() {
-    expect(true).toBeTrue();
-    console.log(init_form());
+it('should calculate the monthly rate correctly', function () {
+  // ...
 });
+
+
+it("should return a result with 2 decimal places", function() {
+  // ..
+});
+
+/// etc
--- calculator.js
+++ calculator.js
@@ -1,68 +1,40 @@
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    init_form();
+window.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById("calc-form");
+  if (form) {
+    setupIntialValues();
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      update();
+    });
+  }
 });
 
-
-
-function init_form() {
-
-    const form = document.getElementById("calc-form");
-
-    if (form) {
-	console.log("in if");
-	return true;	// });
-    }
-	// form.addEventListener("submit", function(e) {
-	//     e.preventDefault();
+function getCurrentUIValues() {
+  return {
+    amount: +(document.getElementById("loan-amount").value),
+    years: +(document.getElementById("loan-years").value),
+    rate: +(document.getElementById("loan-rate").value),
+  }
 }
 
+// Get the inputs from the DOM.
+// Put some default values in the inputs
+// Call a function to calculate the current monthly payment
+function setupIntialValues() {
+}
 
-// function getCurrentUIValues() {
-//     return {
-// 	amount: +(document.getElementById("loan-amount").value),
-// 	years: +(document.getElementById("loan-years").value),
-// 	rate: +(document.getElementById("loan-rate").value),
-//     };
-// }
+// Get the current values from the UI
+// Update the monthly payment
+function update() {
+}
 
+// Given an object of values (a value has amount, years and rate ),
+// calculate the monthly payment.  The output should be a string
+// that always has 2 decimal places.
+function calculateMonthlyPayment(values) {
+}
 
-// // Get the inputs from the DOM.
-// // Put some default values in the inputs
-// // Call a function to caculate the current monthly payment
-// function setupIntialValues() {
-//     document.querySelector('#loan-amount').value = 1000000;
-//     document.querySelector('#loan-years').value = 25;
-//     document.querySelector('#loan-rate').value = 6;
-//     update();
-// }
-
-// // Get the current values from the UI
-// // Update the monthly payment
-// function update() {
-//     const values = getCurrentUIValues();
-//     const monthly = calculateMonthlyPayment(values);
-//     updateMonthly(monthly);
-
-// }
-
-// // Given an object of values (a value has amount, years and rate ),
-// // calculate the monthly payment.  The output should be a string
-// // that always has 2 decimal places.
-// function calculateMonthlyPayment(values) {
-//     const i = (values.rate / 100) / 12;
-//     const n = values.years * 12;
-//     const p = values.amount;
-    
-//     return ((p * i) / (1 - Math.pow((1 + i),-n))).toFixed(2);
-// }
-
-// // Given a string representing the monthly payment value,
-// // update the UI to show the value.
-// function updateMonthly(monthly) {
-//     document.querySelector('#monthly-payment').innerText = monthly;
-// }
-
-
-
-
+// Given a string representing the monthly payment value,
+// update the UI to show the value.
+function updateMonthly(monthly) {
+}
