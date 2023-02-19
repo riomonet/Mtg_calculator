d7dfc0412b52de66ba8bedcc153868706852f040
riomonet <arizablozki@gmail.com>
2023-02-18 22:58:28 -0500

    complete

--- calculator-test.js
+++ calculator-test.js
@@ -1,17 +1,97 @@
-/* included tests for later
-it('should calculate the monthly rate correctly', function () {
-    // ...
+
+describe("A suite to test the mortgage caluculator", function() {
+
+    const rate = .04;
+    const amount = 1000000;
+    const term = 25;
+
+
+
+    it("should get ptrs to input objects from the form: amount, term, rate", function () {
+		expect(setupIntialValues()).toBeTrue();
+    });
+
+
+    it('should validate input as an integer greater than 0', function () {
+
+	setupIntialValues();
+	const input_set = getCurrentValues();
+	expect(validateInput(input_set)).toBeTrue();
+
+    });
+
+    
+    it('should set defaults for each form input element', function () {
+
+	let myObject = { amount: 1000000, term: 25, rate: .04 };
+
+	const amount = document.querySelector("#loan-amount");
+	const term = document.querySelector("#loan-years");
+	const rate = document.querySelector("#loan-rate");
+
+	setDefault(amount, term, rate);
+
+	expect(getCurrentValues()).toEqual(myObject);
+
+    });
+
+    it('should get the  monhtly interest rate', function () {
+
+	expect(getMonthlyRate(rate)).toEqual(rate / 12);
+
+    });
+
+    it('should get the total number of months for the term', function () {
+
+	expect(getTermInMonths(term)).toEqual(term * 12);
+
+    });
+
+
+
+    it('should calculatte the numerator', function () {
+
+	expect(calculateNumerator(amount, rate)).toEqual(amount * (rate / 12));
+
+    });
+
+
+    it('should caculate denominator', function () {
+
+	let i = getMonthlyRate(rate);
+	let n = getTermInMonths(term);
+
+	expect(calculateDenominator(rate, term)).toEqual(1 - ((1 + i) ** -n));
+
+    });
+
+
+    it('calculated Monthly Payment should match know amounts from other calculators', function () {
+
+	setupIntialValues();
+
+	let input_set = getCurrentValues();
+
+	expect(calculateMonthlyPayment(input_set)).toBeCloseTo(5278, 0);
+	
+    });
+
+    it ('should get the valid DOM pbject for Monthly Payment',function(){
+
+	let monthly = 1;
+    
+	expect(updateMonthly(monthly)).toBeTrue();
+    
+    });
+
+
+    it('should get current values and update output', function() {
+
+	expect(update()).toBeTrue();
+	
+    });
 });
 
 
-it("should return a result with 2 decimal places", function() {
-  // ..
-});
-*/
 
 
-it("should get a input object from the DOM that represents Loan amount", function (){
-    expect(setupIntialValues()).toBeTrue();
-});
-
-/// etc
--- calculator.js
+++ calculator.js
@@ -1,45 +1,114 @@
+
+
 window.addEventListener('DOMContentLoaded', function() {
   const form = document.getElementById("calc-form");
-  if (form) {
-    setupIntialValues();
-    form.addEventListener("submit", function(e) {
-      e.preventDefault();
-      update();
+    if (form) {
+	setupIntialValues();
+	form.addEventListener("submit", function(e) {
+	    e.preventDefault();
+	    update();
     });
   }
 });
 
+function setupIntialValues() {
 
+    const amount = document.querySelector("#loan-amount");
+    const term   = document.querySelector("#loan-years");
+    const rate   = document.querySelector("#loan-rate");
 
-function getCurrentUIValues() {
-  return {
-    amount: +(document.getElementById("loan-amount").value),
-    years: +(document.getElementById("loan-years").value),
-    rate: +(document.getElementById("loan-rate").value),
-  };
+    if (amount && term && rate) {
+	setDefault(amount,term,rate);
+	update();
+	    return true;
+    }
 }
 
-// Get the inputs from the DOM.
-// Put some default values in the inputs
-// Call a function to calculate the current monthly payment
-function setupIntialValues() {
-    const amount = document.querySelector("#loan-amount");
-    console.log(amount);
+
+function setDefault(amount,term,rate) {
+    
+    amount.value = 1000000;
+    term.value = 25;
+    rate.value = 4;
+}
+
+
+function getCurrentValues() {
+    return {
+	amount: +document.querySelector("#loan-amount").value,
+	term:   +document.querySelector("#loan-years").value, 
+	rate:   +document.querySelector("#loan-rate").value/100,  
+    };
+
+}
+
+function validateInput(input_set) {
+
+   for (const input in input_set)
+	if ((typeof(input_set[input]) != 'number' || input_set[input] <= 0))
+	    return false;
     return true;
 }
 
-// Get the current values from the UI
-// Update the monthly payment
 function update() {
+
+    let input_set = getCurrentValues();
+
+    if (validateInput(input_set)) {
+	let monthly =  calculateMonthlyPayment(input_set);
+	updateMonthly(`$ ${monthly.toFixed(2)}`);
+	return true;
+    }
+
+    else 
+	updateMonthly('BAD INPUT: INPUT INTEGERS GREATER THAN 0 FOR ALL FIELDS');
+
 }
 
-// Given an object of values (a value has amount, years and rate ),
-// calculate the monthly payment.  The output should be a string
-// that always has 2 decimal places.
-function calculateMonthlyPayment(values) {
-}
 
-// Given a string representing the monthly payment value,
-// update the UI to show the value.
 function updateMonthly(monthly) {
+    const output = document.querySelector('#monthly-payment');
+    if(output) {
+	output.innerHTML = monthly;
+	return true;
+    }
+}
+
+
+function calculateMonthlyPayment(input_set) {
+
+
+    let numerator = calculateNumerator (input_set.amount, input_set.rate);
+    let denominator = calculateDenominator (input_set.rate, input_set.term);
+    
+    return (numerator/denominator);
+}
+
+
+function calculateNumerator(amount, rate) {
+    
+    return amount * getMonthlyRate(rate); 
+}
+
+function calculateDenominator(rate, term){
+
+
+    let i =  getMonthlyRate(rate);
+    let n =  getTermInMonths(term);
+
+    let x = (1+i)**-n;
+
+    return  (1 - x);
+
+}
+
+
+function getMonthlyRate(rate){
+
+    return (rate / 12);
+}
+
+function getTermInMonths(term){
+
+    return (term *  12);
 }
